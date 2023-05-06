7869e6832f1dd6340f39a541a062b2d40f0b7cfe
s.m.israt <smisratara25@gmail.com>
2023-05-06 10:47:12 +0600

    style for textfield added

---
+++ lib/ToDo_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:to_do_app/style.dart';
+
+class ToDo_page extends StatefulWidget {
+  const ToDo_page({Key? key}) : super(key: key);
+
+  @override
+  State<ToDo_page> createState() => _ToDo_pageState();
+}
+
+class _ToDo_pageState extends State<ToDo_page> {
+  List ToDoList=[];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("To Do APP"),),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Expanded(flex:10,child: Row(
+              children: [
+                Expanded(flex:70,child: TextField(decoration: AppInputDecoration("List Item"),)),
+                Expanded(flex:30,child: ElevatedButton(onPressed: (){}, child: Text("ADD"),))
+              ],
+            )),
+            Expanded(flex:90,
+                child: ListView.builder(
+                  itemCount: ToDoList.length,
+                itemBuilder: (context, index) {
+                  return Card();
+                }))
+
+          ],
+        ),
+      ),
+    );
+
+  }
+}
---
+++ lib/style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+InputDecoration AppInputDecoration(label)
+{
+  return InputDecoration(
+    contentPadding: EdgeInsets.fromLTRB(20, 10, 10, 20),
+    border: OutlineInputBorder(),
+labelText: label
+  );
+}
